f506a934266bc412fe25d7409c8d92183e1daa54
Thomas H Jones II <github@xanthia.com>
2015-10-22 14:09:13 +0000

    Change package install-method * Removed cmd.run state-type in favor of cmd.script state-type * Move package install to external script   * Build in 'do I need to do this' logic   * Build in exits to support use of 'stateful' token

---
+++ join-domain/elx/files/pbis-install_only.sh
@@ -0,0 +1,31 @@
+#!/bin/sh
+
+#
+# Salt state for downloading, installing and configuring PBIS,
+# then joining # the instance to Active Directory
+#
+#################################################################
+PBISPKG="${1:-UNDEF}"
+INSTPBISVERS="$(rpm --qf '%{version}\n' -q pbis-open)"
+ISINSTALLED=$(echo "${PBISPKG}" | grep -- "-${INSTPBISVERS}.")
+
+
+if [[ "${INSTPBISVERS}" = "" ]] || [[ "${ISINSTALLED}" = "" ]]
+then
+   bash /var/tmp/${PBISPKG} -- --dont-join --legacy install > /dev/null 2>&1
+   if [[ $? -eq 0 ]]
+   then
+      printf "\n"
+      printf "changed=yes comment='Installed RPMs from ${PBISPKG}.'\n"
+      exit 0
+   else
+      printf "\n"
+      printf "changed=no comment='Installer ${PBISPKG} did not run "
+      printf "as expected.'\n"
+      exit 1
+   fi
+else
+   printf "\n"
+   printf "changed=no comment='RPMs from ${PBISPKG} already present.'\n"
+   exit 0
+fi
