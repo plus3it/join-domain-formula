570417f4481cfd11fe772b81bf7185787501e601
Thomas H Jones II <github@xanthia.com>
2023-02-07 19:52:53 +0000

    Decrypt password-string and supply to realm command

--- join-domain/elx/sssd/files/join.sh
+++ join-domain/elx/sssd/files/join.sh
@@ -9,6 +9,8 @@ JOIN_DOMAIN="${JOIN_DOMAIN:-UNDEF}"
 JOIN_OU="${JOIN_OU:-}"
 JOIN_USER="${JOIN_USER:-Administrator}"
 JOIN_CNAME="${JOIN_CNAME:-UNDEF}"
+PWCRYPT="${ENCRYPT_PASS:-UNDEF}"
+PWUNLOCK="${ENCRYPT_KEY:-UNDEF}"
 CLIENT_OSNAME="$(
    awk -F "=" '/^NAME/{ print $2}' /etc/os-release |
    sed 's/"//g'
@@ -18,6 +20,24 @@ CLIENT_OSVERS="$(
    sed 's/"//g'
 )"
 
+# Get clear-text password from crypt
+function PWdecrypt {
+  local PWCLEAR
+
+  PWCLEAR=$(
+     echo "${PWCRYPT}" | \
+     openssl enc -aes-256-cbc -md sha256 -a -d -salt -pass pass:"${PWUNLOCK}"
+  )
+
+  if [[ $? -ne 0 ]]
+  then
+    echo ""
+    return 1
+  else
+    echo "${PWCLEAR}"
+    return 0
+  fi
+}
 
 # Make sure domain is discoverable
 function IsDiscoverable {
@@ -36,13 +56,15 @@ function JoinDomain {
 
    if [[ -z ${JOIN_OU} ]]
    then
+      echo "$( PWdecrypt )" | \
       realm join -U "${JOIN_USER}" \
         --os-name="${CLIENT_OSNAME}" \
         --os-version="${CLIENT_OSVERS}" "${JOIN_DOMAIN}"
    elif [[ -n ${JOIN_OU} ]]
    then
+      echo "$( PWdecrypt )" | \
       realm join -U "${JOIN_USER}" \
-	--computer-ou="${JOIN_OU}" \
+        --computer-ou="${JOIN_OU}" \
         --os-name="${CLIENT_OSNAME}" \
         --os-version="${CLIENT_OSVERS}" "${JOIN_DOMAIN}"
    else
