03662b42cd2b05eb1861db05c202e054e0388c5e
Thomas H Jones II <github@xanthia.com>
2023-02-08 15:45:23 -0500

    Convert sw from 3 to 2
    (yuck)

--- join-domain/elx/sssd/files/join.sh
+++ join-domain/elx/sssd/files/join.sh
@@ -11,100 +11,100 @@ JOIN_CNAME="${JOIN_CNAME:-UNDEF}"
 PWCRYPT="${ENCRYPT_PASS:-UNDEF}"
 PWUNLOCK="${ENCRYPT_KEY:-UNDEF}"
 CLIENT_OSNAME="$(
-   awk -F "=" '/^NAME/{ print $2}' /etc/os-release |
-   sed 's/"//g'
+  awk -F "=" '/^NAME/{ print $2}' /etc/os-release |
+  sed 's/"//g'
 )"
 CLIENT_OSVERS="$(
-   awk -F "=" '/^VERSION_ID/{ print $2 }' /etc/os-release |
-   sed 's/"//g'
+  awk -F "=" '/^VERSION_ID/{ print $2 }' /etc/os-release |
+  sed 's/"//g'
 )"
 
 # Get clear-text password from crypt
 function PWdecrypt {
-   local PWCLEAR
+  local PWCLEAR
 
-   # Get cleartext password-string
-   PWCLEAR=$(
-      echo "${PWCRYPT}" | \
-      openssl enc -aes-256-cbc -md sha256 -a -d -salt -pass pass:"${PWUNLOCK}"
-   )
+  # Get cleartext password-string
+  PWCLEAR=$(
+    echo "${PWCRYPT}" | \
+    openssl enc -aes-256-cbc -md sha256 -a -d -salt -pass pass:"${PWUNLOCK}"
+  )
 
-   # This sucks, but...
-   # shellcheck disable=SC2181
-   if [[ $? -ne 0 ]]
-   then
-      echo ""
-      return 1
-   else
-      echo "${PWCLEAR}"
-      return 0
-   fi
+  # This sucks, but...
+  # shellcheck disable=SC2181
+  if [[ $? -ne 0 ]]
+  then
+    echo ""
+    return 1
+  else
+    echo "${PWCLEAR}"
+    return 0
+  fi
 }
 
 # Make sure domain is discoverable
 function IsDiscoverable {
-   if [[ $( realm discover "${JOIN_DOMAIN}" > /dev/null 2>&1 )$? -eq 0 ]]
-   then
-      printf "The %s domain is discoverable\n" "${JOIN_DOMAIN}"
-      return 0
-   else
-      printf "The %s domain is not discoverable. Aborting...\n" "${JOIN_DOMAIN}"
-      return 1
-   fi
+  if [[ $( realm discover "${JOIN_DOMAIN}" > /dev/null 2>&1 )$? -eq 0 ]]
+  then
+    printf "The %s domain is discoverable\n" "${JOIN_DOMAIN}"
+    return 0
+  else
+    printf "The %s domain is not discoverable. Aborting...\n" "${JOIN_DOMAIN}"
+    return 1
+  fi
 }
 
 # Try to join host to domain
 function JoinDomain {
 
-   # Toggle SELinux if necessary
-   if [[ $( getenforce ) == "Enforcing" ]]
-   then
-      SEL_TARG="1"
-      printf "Toggling SELinux mode... "
-      setenforce 0 || (echo "FAILED" ; exit 1 )
-      echo SUCCESS
-   else
-      SEL_TARG=0
-   fi
+  # Toggle SELinux if necessary
+  if [[ $( getenforce ) == "Enforcing" ]]
+  then
+    SEL_TARG="1"
+    printf "Toggling SELinux mode... "
+    setenforce 0 || (echo "FAILED" ; exit 1 )
+    echo SUCCESS
+  else
+    SEL_TARG=0
+  fi
 
-   if [[ -z ${JOIN_OU} ]]
-   then
-      printf "Joining to %s... " "${JOIN_DOMAIN}"
-      # shellcheck disable=SC2005
-      echo "$( PWdecrypt )" | \
-      realm join -U "${JOIN_USER}" \
-         --unattended \
-         --os-name="${CLIENT_OSNAME}" \
-         --os-version="${CLIENT_OSVERS}" "${JOIN_DOMAIN}" > /dev/null 2>&1 || \
-      ( echo "FAILED" ; exit 1)
-      echo "Success"
+  if [[ -z ${JOIN_OU} ]]
+  then
+    printf "Joining to %s... " "${JOIN_DOMAIN}"
+    # shellcheck disable=SC2005
+    echo "$( PWdecrypt )" | \
+    realm join -U "${JOIN_USER}" \
+      --unattended \
+      --os-name="${CLIENT_OSNAME}" \
+      --os-version="${CLIENT_OSVERS}" "${JOIN_DOMAIN}" > /dev/null 2>&1 || \
+    ( echo "FAILED" ; exit 1)
+    echo "Success"
 
-   elif [[ -n ${JOIN_OU} ]]
-   then
-      printf "Joining to %s under %s OU... " "${JOIN_DOMAIN}" "${JOIN_OU}"
-      # shellcheck disable=SC2005
-      echo "$( PWdecrypt )" | \
-      realm join -U "${JOIN_USER}" \
-         --unattended \
-         --computer-ou="${JOIN_OU}" \
-         --os-name="${CLIENT_OSNAME}" \
-         --os-version="${CLIENT_OSVERS}" "${JOIN_DOMAIN}" > /dev/null 2>&1 || \
-      ( echo "FAILED" ; exit 1)
-      echo "Success"
-   else
-      echo "Unsupported configuration-options"
-      return 1
-   fi
+  elif [[ -n ${JOIN_OU} ]]
+  then
+    printf "Joining to %s under %s OU... " "${JOIN_DOMAIN}" "${JOIN_OU}"
+    # shellcheck disable=SC2005
+    echo "$( PWdecrypt )" | \
+    realm join -U "${JOIN_USER}" \
+      --unattended \
+      --computer-ou="${JOIN_OU}" \
+      --os-name="${CLIENT_OSNAME}" \
+      --os-version="${CLIENT_OSVERS}" "${JOIN_DOMAIN}" > /dev/null 2>&1 || \
+    ( echo "FAILED" ; exit 1)
+    echo "Success"
+  else
+    echo "Unsupported configuration-options"
+    return 1
+  fi
 
-   # Revert SEL as necessary
-   if [[ ${SEL_TARG} -eq 1 ]]
-   then
-      printf "Resetting SELinux mode... "
-      setenforce "${SEL_TARG}" || ( echo "FAILED" ; exit 1 )
-      echo "Success"
-   fi
+  # Revert SEL as necessary
+  if [[ ${SEL_TARG} -eq 1 ]]
+  then
+    printf "Resetting SELinux mode... "
+    setenforce "${SEL_TARG}" || ( echo "FAILED" ; exit 1 )
+    echo "Success"
+  fi
 
-   return 0
+  return 0
 }
 
 IsDiscoverable
