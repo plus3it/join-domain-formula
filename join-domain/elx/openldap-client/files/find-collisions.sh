0f724404b33b2144dada962eb5d95a0fed171806
Thomas H Jones II <github@xanthia.com>
2023-07-28 05:31:33 -0400

    Add err_exit function

--- join-domain/elx/openldap-client/files/find-collisions.sh
+++ join-domain/elx/openldap-client/files/find-collisions.sh
@@ -12,13 +12,42 @@ CRYPTSTRING="${CRYPTSTRING:-}"
 DEBUGVAL="${DEBUG:-false}"
 DIR_DOMAIN="${JOIN_DOMAIN:-}"
 DIRUSER="${JOIN_USER:-}"
-DOEXIT="0"
 DOMAINNAME="${JOIN_DOMAIN:-}"
 DS_LIST=()
 LDAPTYPE="AD"
 LOGFACIL="user.err"
 REQ_TLS="${REQ_TLS:-true}"
 
+# Make interactive-execution more-verbose unless explicitly told not to
+if [[ $( tty -s ) -eq 0 ]] && [[ ${DEBUG} == "UNDEF" ]]
+then
+   DEBUG="true"
+fi
+
+# Error handler function
+function err_exit {
+   local ERRSTR
+   local ISNUM
+   local SCRIPTEXIT
+
+   ERRSTR="${1}"
+   ISNUM='^[0-9]+$'
+   SCRIPTEXIT="${2:-1}"
+
+   if [[ ${DEBUG} == true ]]
+   then
+      # Our output channels
+      logger -i -t "${PROGNAME}" -p kern.crit -s -- "${ERRSTR}"
+   else
+      logger -i -t "${PROGNAME}" -p kern.crit -- "${ERRSTR}"
+   fi
+
+   # Only exit if requested exit is numerical
+   if [[ ${SCRIPTEXIT} =~ ${ISNUM} ]]
+   then
+      exit "${SCRIPTEXIT}"
+   fi
+}
 
 # Get Candidate DCs
 function CandidateDirServ {
